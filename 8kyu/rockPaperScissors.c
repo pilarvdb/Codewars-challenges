04c9f2f058a6511b9014aea9156fb48382d334ff
Pili Van Der Borght <pili.vanderborght@student.kuleuven.be>
2023-11-07 14:56:24 +0100

    C challenge level 8: rock paper scissors

---
+++ 8kyu/rockPaperScissors.c
@@ -0,0 +1,45 @@
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// My solution
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+enum tool {ROCK, PAPER, SCISSORS};
+enum outcome {P1_WON, P2_WON, DRAW};
+
+enum outcome rps (enum tool p1, enum tool p2)
+{
+  if (p1 == p2){
+	  return DRAW;
+    }
+  if (p1 == ROCK){
+    if (p2 == PAPER){
+      return P2_WON;
+    }
+    else{
+      return P1_WON;
+    }
+  }
+  if (p1 == PAPER){
+    if (p2 == ROCK){
+      return P1_WON;
+    }
+    else {
+      return P2_WON;
+    }
+  }
+  else {
+    if (p2 == ROCK){
+      return P2_WON;
+    }
+    else{
+      return P1_WON;
+    }
+  }
+}
